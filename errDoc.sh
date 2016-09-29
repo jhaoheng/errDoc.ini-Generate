b3f74a5eac1607c25e500e09e7de5b63e43f61df
maxhu <jhaoheng@gmail.com>
2016-09-29 14:01:20 +0800

    update

--- errDoc.sh
+++ errDoc.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'line=($(cat ./word.ini))'
+ini_file_path="./word.ini"
+
+exportFile="errCode_doc.md"
+exportPath="./"
+output=$exportPath$exportFile
+
+echo "input file : "$ini_file_path
+echo "output file : "$output
+echo ""
+echo "============"
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'line=($(cat $ini_file_path))'
 
 exportFile="errCode_doc.md"
 rm errCode_doc.md
 
-maxLine=$(cat word.ini | wc -l);
+maxLine=$(cat $ini_file_path | wc -l);
 # echo $maxLine;
 # echo "${line[0]}"
 
@@ -17,15 +28,15 @@ for (( i = 0; i < $maxLine; i++ )); do
 	lastC=${line[$i]: -1};
 
 	if [ "$firstC" == "[" ] && [ "$lastC" == "]" ]; then
-		echo " " | tee -a $exportFile
-		echo ${line[$i]} | tee -a $exportFile
+		echo " " | tee -a $output
+		echo ${line[$i]} | tee -a $output
 
 	else
 		content=${line[$i]}
-		echo ${content##*=} | tee -a $exportFile
+		echo ${content##*=} | tee -a $output
 	fi
 
 done
 
 echo "============"
-echo "Export file name is : "$exportFile
+echo "Export file name is : "$output
