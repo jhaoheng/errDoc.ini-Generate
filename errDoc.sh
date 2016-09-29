b3c1566661ba28c212aa149974b1568d0df47f76
maxhu <jhaoheng@gmail.com>
2016-09-29 13:24:56 +0800

    init

---
+++ errDoc.sh
@@ -0,0 +1,23 @@
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'line=($(cat ./word.ini))'
+
+maxLine=$(cat word.ini | wc -l);
+# echo $maxLine;
+# echo "${line[0]}"
+
+for (( i = 0; i < $maxLine; i++ )); do
+	# echo ${line[$i]:0:1};
+	# echo ${line[$i]: -1};
+
+	firstC=${line[$i]:0:1};
+	lastC=${line[$i]: -1};
+
+	if [ "$firstC" == "[" ] && [ "$lastC" == "]" ]; then
+		echo " " | tee -a output.md
+		echo ${line[$i]} | tee -a output.md
+
+	else
+		content=${line[$i]}
+		echo ${content##*=} | tee -a output.md
+	fi
+
+done
